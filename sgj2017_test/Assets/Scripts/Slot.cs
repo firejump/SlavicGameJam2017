a9c2fd20988e058b94dd9edf600174e98099b6db
Anna Olchowik <ania.olchowik@gmail.com>
2017-07-29 19:20:05 +0200

    tag for lock button

---
+++ sgj2017_test/Assets/Scripts/Slot.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Slot : MonoBehaviour {
+	private bool active = true;
+	private Text text;
+	private Button button;
+	// Use this for initialization
+	void Start () {
+		text = GetComponentInChildren<Text> ();
+		button = gameObject.GetComponent<Button> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void StartMachine(){
+		button.interactable = false;
+		
+	} 
+	public void StopMachine(){
+	}
+	public void ToggleSlotMachine(){
+		active = !active;
+		text.text = active ? "Free": "Locked";
+	}
+}
