be8bcd0105deeb15790971408d01097ff68a527f
Piotr Iwaniuk <piwaniuk@poczta.onet.pl>
2017-07-29 18:12:39 +0200

    Add obstacle handlers

---
+++ sgj2017_test/Assets/Scripts/ObstacleBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleBehaviour : MonoBehaviour {
+    public string handler = "";
+
+    private Dictionary<string, ObstacleHandler> HANDLERS = new Dictionary<string, ObstacleHandler>()
+    {
+        { "ravine", new RavineHandler() },
+        { "flower", new FlowerHandler() },
+        { "slope", new SlopeHandler() }
+    };
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        ObstacleHandler handlerObject = HANDLERS[handler];
+        UnityEngine.Debug.Log("CHECK!");
+        if (handlerObject.check())
+        {
+            UnityEngine.Debug.Log("GAME OVER");
+            //TODO call some GameState.getInstance().gameOver();
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ sgj2017_test/Assets/Scripts/ObstacleHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public abstract class ObstacleHandler
+{
+    public abstract bool check();
+}
+
+public class RavineHandler : ObstacleHandler {
+    public override bool check()
+    {
+        return GameState.getInstance().getPlayerState().checkFeature("size", "l");
+    }
+}
+
+public class FlowerHandler : ObstacleHandler
+{
+    public override bool check()
+    {
+        return !GameState.getInstance().getPlayerState().checkFeature("color", "green");
+    }
+}
+
+public class SlopeHandler : ObstacleHandler
+{
+    public override bool check()
+    {
+        PlayerState state = GameState.getInstance().getPlayerState();
+        return !(state.checkFeature("shape", "ball") || state.checkFeature("character", "energetic"));
+    }
+}
